60dff9e01b5d01511e23cb2fd74b574896758439
geisniwander <geisniwander@gmail.com>
2023-04-10 12:44:18 -0300

    refactor: typing errors functions

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,11 +1,22 @@
-function conflictError(message: string[]) {
+interface Error {
+  name:string,
+  message:string, 
+}
+
+interface ErrorEmail {
+  name:string,
+  message:string, 
+  email: string
+}
+
+function conflictError(message: string[]): Error {
   return {
     name: "ConflictError",
-    message,
+    message: "Conflito",
   };
 }
 
-function ConflictDateError(message: Date) {
+function ConflictDateError(message: Date): Error  {
   return {
     name: "ConflictDateError",
     message: `A data/horário não estão disponíveis (consulta agendada no horário ou fora do horário de atendimento). Lembre-se: o horário de funcionamento é entre
@@ -13,7 +24,7 @@ function ConflictDateError(message: Date) {
   };
 }
 
-function duplicatedEmailError(email: string) {
+function duplicatedEmailError(email: string): ErrorEmail  {
   return {
     name: "DuplicatedEmailError",
     message: "O email informado já está sendo utilizado!",
@@ -21,28 +32,28 @@ function duplicatedEmailError(email: string) {
   };
 }
 
-function unauthorizedError() {
+function unauthorizedError(): Error  {
   return {
     name: "UnauthorizedError",
     message: "Você precisa realizar login para continuar",
   };
 }
 
-function notFoundError() {
+function notFoundError(): Error  {
   return {
     name: "NotFoundError",
     message: "Não encontrado!",
   };
 }
 
-function invalidCredentialsError() {
+function invalidCredentialsError(): Error  {
   return {
     name: "InvalidCredentialsError",
     message: "Credenciais incorretas",
   };
 }
 
-function invalidAppointmentError() {
+function invalidAppointmentError(): Error  {
   return {
     name: "InvalidAppointmentError",
     message: "Consulta não encontrada",
